eedf8fecce3d806014e76c2afde63ea783300a12
Scott Green <sagreenxyz@gmail.com>
2022-03-28 16:32:44 -0500

    5. applying stying context to CollectorButton and RequestFormButton components

--- src/CollectorButton.jsx
+++ src/CollectorButton.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
-
+import StylesContext from "./StylesContext";
 
 export default function CollectorButton() {
-  
+  const buttonStyling = React.useContext(StylesContext)
   return (
     <div>
-      <button>
+      <button style={buttonStyling}>
         Click Here!
       </button>
     </div>
--- src/RequestFormButton.jsx
+++ src/RequestFormButton.jsx
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
-
+import StylesContext from "./StylesContext";
 
 export default function RequestFormButton() {
+  const buttonStyling = React.useContext(StylesContext)
   return (
     <div>
-      <button>
+      <button style={buttonStyling}>
         Click Here!
       </button>
       <input type="submit" value="Submit" />
